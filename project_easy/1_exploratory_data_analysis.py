28d0ed2d947bcf6504eaf44385d33f0f7d6f26e0
Christian Camp <crdcamp@gmail.com>
2026-01-15 16:57:17 -0700

    MSE, mean absolute error, and r2 score

--- project_easy/1_exploratory_data_analysis.py
+++ project_easy/1_exploratory_data_analysis.py
@@ -8,7 +8,7 @@ import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn import preprocessing
 
 plt.style.use('dark_background')
@@ -173,3 +173,7 @@ study_sleep_model = LinearRegression()
 study_sleep_model.fit(X_train, y_train)
 
 sleep_study_predictions = study_sleep_model.predict(X_test)
+
+print("Mean Squared Error", mean_squared_error(y_test, sleep_study_predictions))
+print("Mean Absolute Error", mean_absolute_error(y_test, sleep_study_predictions))
+print("R2 score", r2_score(y_test, sleep_study_predictions))
