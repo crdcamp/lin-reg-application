12f296bc8c97045315fc5563c56cb4276e2ad6ce
Christian Camp <crdcamp@gmail.com>
2026-02-26 16:26:14 -0700

    Initial data preparation (splitting and encoding categorical variables)

--- project_easy/2_linear_regression.py
+++ project_easy/2_linear_regression.py
@@ -21,4 +21,33 @@ pd.set_option('display.max_colwidth', None)
 # No need for data cleaning since we're working with easy data that doesn't require it
 df_student = pd.read_csv("../data/Student_Performance.csv")
 
-# %% Train-test split
+# %% Data Preparation
+# Split data into the features (independent variables) and target (dependent variable)
+X, y = df_student.drop(columns=["Performance Index"]), df_student["Performance Index"]
+print("X shape: ", X.shape)
+print("y shape: ", y.shape, "\n")
+
+# Encode the categorical variable `Extracurricular Activities`
+X = pd.get_dummies(X, columns=["Extracurricular Activities"], drop_first=True)
+
+# Split into train and test data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+print("X_train, X_test shape: ", X_train.shape, X_test.shape)
+print("y_train, y_test shape: ", y_train.shape, y_test.shape, "\n")
+
+# Inspect data types before continuing (just in case)
+print("X dtypes:\n", X.dtypes, "\n")
+print("y dtype:\n", y.dtypes, "\n")
+
+# %% statsmodels Model Creation
+"""
+Now it's time to start creating some models!
+
+We'll begin with a multiple linear regression model using all variables in the predictions
+using the statsmodels library.
+
+Then, we'll thoroughly inspect the data with all the tools statsmodels provides in order
+to brush up on interpreting linear regression models.
+""";
