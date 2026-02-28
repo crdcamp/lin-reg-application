809595260e248df1670f1bdc11a6eb121e2dfac4
Christian Camp <crdcamp@gmail.com>
2026-02-28 13:29:26 -0700

    Cleaning up data preparation (again...)

--- project_easy/2_linear_regression.py
+++ project_easy/2_linear_regression.py
@@ -1,31 +1,33 @@
-# %% Imports, options, and original data
+# %% Imports, options, and loading data
 import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import statsmodels
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from statsmodels.api import OLS
+from statsmodels.formula.api import ols
 
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-plt.style.use('dark_background') # Matches the GitHub theme on my laptop better :)
-
+# pandas display options
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
 
-# No need for data cleaning since we're working with easy data that doesn't require it
+# matplotlib display options
+plt.style.use('dark_background') # Matches the GitHub theme on my laptop better :)
+
+# Load in data and remove white space
 df_student = pd.read_csv("../data/Student_Performance.csv")
-# Actually... let's remove the white space:
 df_student.columns = df_student.columns.str.replace(' ', '', regex=False)
+
 
 # %% Data Preparation
 # Split data into the features (independent variables) and target (dependent variable)
@@ -35,18 +37,12 @@ print("y shape: ", y.shape, "\n")
 
 # Encode the categorical variable `ExtracurricularActivities` before train/test split
 X = pd.get_dummies(X, columns=["ExtracurricularActivities"], drop_first=True) # `drop_first=True` because we're dealing with a simple binary category
+
+# Convert and inspect data types
 X = X.astype(float)
-
-# Ensure data shapes are consistent
-for name, var in [("X_train shape: ", X_train),
-    ("X_test shape: ", X_test),
-    ("y_train shape: ", y_train),
-    ("y_test shape: ", y_test)]:
-    print(name, var.shape)
-
-# Inspect data types
 print("\nX dtypes:\n", X.dtypes, "\n")
 print("y dtype:\n", y.dtypes, "\n")
+
 
 # %% statsmodels Model Creation
 """
@@ -63,24 +59,28 @@ Then, we'll thoroughly inspect the data with all the tools statsmodels provides 
 to brush up on interpreting linear regression models.
 """;
 
-# Add a constant and split the data
-X = sm.add_constant(X)
+# Split the data
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-print("Note: X variables have a different shape due to the addition of the constant.")
+# Ensure data shapes are consistent
 for name, var in [("X_train shape: ", X_train),
     ("X_test shape: ", X_test),
     ("y_train shape: ", y_train),
     ("y_test shape: ", y_test)]:
     print(name, var.shape)
-print()
+
+# Add constant and create/fit the model
+X_train, X_test = sm.add_constant(X_train), sm.add_constant(X_test)
+print("X_train shape after constant: ", X_train.shape)
+print("X_test shape after constant: ", X_test.shape)
 
 sm_model = sm.OLS(y_train, X_train) # Reminder that statsmodels uses Y followed by X
 sm_results = sm_model.fit()
 
-# %% Interpreting statsmodels OLS Results
+
+# %% statsmodels OLS Results
 # General information
 print("\nSTATSMODELS MODEL RESULTS")
 print("Coefficients:\n", sm_results.params, "\n")
@@ -113,7 +113,8 @@ This one uses only PreviousScores, for example:"""
 example_formula_model = smf.ols(formula="PerformanceIndex ~ PreviousScores", data=df_student)
 example_formula_model_results = example_formula_model.fit()
 
-# %% Plots for Initial Model Results
+
+# %% Plot statsmodels results
 """
 Now it's time to interpret these results. We'll begin by graphing them
 and checking out those results.
@@ -125,25 +126,20 @@ have to keep in mind that these will be important in a more advanced application
 Here's where you can find all the regression plots for statsmodels:
 https://www.statsmodels.org/stable/graphics.html#regression-plots
 """;
-# From https://www.statology.org/residual-plot-python/
-# Inspect different exogenous (independent) variables
+
+# I'll start by making functions for each statsmodels graphics plot option
+# I'll just leave them here in case I want to do a deep dive into them
+
+# Use this list for function calls
 X_columns = X.columns.drop('const')
 
-# Might use this later on X_columns
-def plot_regress_exog(model_results: object, feature: str):
-    fig = sm.graphics.plot_regress_exog(model_results, feature, fig=fig)
-    plt.show();
+# Influence Plot
+fig, ax = plt.subplots(figsize=(12, 8))
+fig = sm.graphics.influence_plot(sm_results, criterion='cooks', ax=ax)
+plt.show();
 
-def plot_regress_fit(model_results: object, feature: str):
-    fig = statsmodels.graphics.regressionplots.plot_fit(model_results, feature)
-    plt.show();
-
-for col in X_columns:
-    plot_regress_fit(sm_results, col)
-
-plt.show()
-# Actual vs. predicted plot
 
 # %% Interpreting the Summary Results
+
 
 # %% Testing AIC and BIC with different feature combinations (for practice)
