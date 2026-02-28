f3b7985b5e4920b9ec8651677451e44709bccf72
Christian Camp <crdcamp@gmail.com>
2026-02-28 12:24:31 -0700

    Some serious data handling streamlining and added regression results plot

--- project_easy/2_linear_regression.py
+++ project_easy/2_linear_regression.py
@@ -34,13 +34,9 @@ print("y shape: ", y.shape, "\n")
 
 # Encode the categorical variable `ExtracurricularActivities` before train/test split
 X = pd.get_dummies(X, columns=["ExtracurricularActivities"], drop_first=True)
+X = X.astype(float)
 
-# Split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-# Split into train and test data
+# Ensure data shapes are consistent
 for name, var in [("X_train shape: ", X_train),
     ("X_test shape: ", X_test),
     ("y_train shape: ", y_train),
@@ -58,35 +54,29 @@ Now it's time to start creating some models!
 We'll begin with a multiple linear regression model using all variables in the predictions
 using the statsmodels library.
 
+Then, we'll test out different variations of a multiple linear regression model with statsmodels
+and further test potential relationships between features. For instance, in `1_exploratory_data_analysis.py`,
+we were only testing for Pearson (linear) correlations. We want to test for other possible correlations as well.
+
 Then, we'll thoroughly inspect the data with all the tools statsmodels provides in order
 to brush up on interpreting linear regression models.
-
-Before we continue though, let's make all of the previously created variables into numpy data types
-to make statsmodels's extremely inflexible data type handling happy.
-
-We'll assign them separately to avoid confusion later and keep the feature and target variable
-column names for the scikit application.
 """;
 
-X_np, y_np = X.astype(float), y.astype(float)
-X_np, y_np = X_np.to_numpy(), y_np.to_numpy()
-
-X_np = sm.add_constant(X_np)
-
-# Do a separate split for the converted values
-X_np_train, X_np_test, y_np_train, y_np_test = train_test_split(
-    X_np, y_np, test_size=0.2, random_state=42
+# Add a constant and split the data
+X = sm.add_constant(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
 )
 
-print("Note: X_np variables have a different shape due to the addition of the constant.")
-for name, var in [("X_np_train shape: ", X_np_train),
-    ("X_np_test shape: ", X_np_test),
-    ("y_np_train shape: ", y_np_train),
-    ("y_np_test shape: ", y_np_test)]:
+print("Note: X variables have a different shape due to the addition of the constant.")
+for name, var in [("X_train shape: ", X_train),
+    ("X_test shape: ", X_test),
+    ("y_train shape: ", y_train),
+    ("y_test shape: ", y_test)]:
     print(name, var.shape)
 print()
 
-sm_model = sm.OLS(y_np_train, X_np_train) # Reminder that statsmodels uses Y followed by X
+sm_model = sm.OLS(y_train, X_train) # Reminder that statsmodels uses Y followed by X
 sm_results = sm_model.fit()
 
 # %% Interpreting statsmodels OLS Results
@@ -97,20 +87,20 @@ print(sm_results.summary(), "\n")
 
 # Train vs. test data information
 # Also, who would've thought... the sklearn metrics are compatible with the statsmodels results!
-print("R^2 Score (Training): ", metrics.r2_score(y_np_train, sm_results.predict(X_np_train)))
-print("R^2 Score (Testing): ", metrics.r2_score(y_np_test, sm_results.predict(X_np_test)), "\n")
+print("R^2 Score (Training): ", metrics.r2_score(y_train, sm_results.predict(X_train)))
+print("R^2 Score (Testing): ", metrics.r2_score(y_test, sm_results.predict(X_test)), "\n")
 
 # Note: Will use sklearn MSE import later in the code
 
 # Training data squared error and mean squared error
-sm_model_train_pred = sm_results.predict(X_np_train)
-sm_model_train_se = (sm_model_train_pred - y_np_train)**2
+sm_model_train_pred = sm_results.predict(X_train)
+sm_model_train_se = (sm_model_train_pred - y_train)**2
 sm_model_train_mse = sm_model_train_se.mean()
 print("Train Data Mean Squared Error: ", sm_model_train_mse)
 
 # Test data squared error and mean squared error
-sm_model_test_pred = sm_results.predict(X_np_test)
-sm_model_test_se = (sm_model_test_pred - y_np_test)**2
+sm_model_test_pred = sm_results.predict(X_test)
+sm_model_test_se = (sm_model_test_pred - y_test)**2
 sm_model_test_mse = sm_model_test_se.mean()
 print("Test Data Mean Squared Error: ", sm_model_test_mse)
 
@@ -131,6 +121,12 @@ After that, let's type some notes for all these results from the summary we're a
 While the data we're working with here is incredibly simple, we also
 have to keep in mind that these will be important in a more advanced application.
 """;
+# Residuals plot
+fig = plt.figure(figsize=(12, 8))
+fig = sm.graphics.plot_regress_exog(sm_results, 'PreviousScores', fig=fig)
+plt.show()
+
+# Actual vs. predicted plot
 
 # %% Interpreting the Summary Results
 
